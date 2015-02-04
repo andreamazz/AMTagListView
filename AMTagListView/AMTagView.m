c776a0f1d6f1d0656520f2ec12667663e192e36a
Felipe Baytelman <felipe.baytelman@gmail.com>
2015-02-03 17:54:31 -0800

    Fix radius: radius is reduced by padding (not by half). Also, skip background if padding is ZERO

--- AMTagListView/AMTagView.m
+++ AMTagListView/AMTagView.m
@@ -120,35 +120,37 @@ NSString * const AMTagViewNotification = @"AMTagViewNotification";
 
 - (void)drawWithTagForRect:(CGRect)rect
 {
+    float padding = self.innerTagPadding;
     float tagLength = self.tagLength;
     float height = rect.size.height;
     float width = rect.size.width;
     float radius = self.radius;
     
-    UIBezierPath *aPath = [UIBezierPath bezierPath];
+    if (padding > 0) {
+        UIBezierPath *aPath = [UIBezierPath bezierPath];
+        
+        [aPath moveToPoint:(CGPoint){width, height / 2}];
+        [aPath addLineToPoint:CGPointMake(width, radius)];
+        [aPath addArcWithCenter:(CGPoint){width - radius, radius} radius:radius startAngle:DEGREES_TO_RADIANS(0) endAngle:DEGREES_TO_RADIANS(270) clockwise:NO];
+        [aPath addLineToPoint:(CGPoint){tagLength + radius, 0.0}];
+        [aPath addArcWithCenter:(CGPoint){tagLength + radius, radius} radius:radius startAngle:DEGREES_TO_RADIANS(270) endAngle:DEGREES_TO_RADIANS(230) clockwise:NO];
+        [aPath addLineToPoint:(CGPoint){0.0, height / 2}];
+        
+        [aPath moveToPoint:(CGPoint){tagLength / 2, height / 2}];
+        [aPath addArcWithCenter:(CGPoint){tagLength / 2 + self.holeRadius, height / 2} radius:self.holeRadius startAngle:DEGREES_TO_RADIANS(180) endAngle:DEGREES_TO_RADIANS(0) clockwise:YES];
+        
+        UIBezierPath *p2 = [UIBezierPath bezierPathWithCGPath:aPath.CGPath];
+        [p2 applyTransform:CGAffineTransformMakeScale(1, -1)];
+        [p2 applyTransform:CGAffineTransformMakeTranslation(0, height)];
+        [aPath appendPath:p2];
+        
+        // Set the render colors.
+        [self.tagColor setFill];
+        
+        [aPath fill];
+    }
     
-    [aPath moveToPoint:(CGPoint){width, height / 2}];
-    [aPath addLineToPoint:CGPointMake(width, radius)];
-    [aPath addArcWithCenter:(CGPoint){width - radius, radius} radius:radius startAngle:DEGREES_TO_RADIANS(0) endAngle:DEGREES_TO_RADIANS(270) clockwise:NO];
-    [aPath addLineToPoint:(CGPoint){tagLength + radius, 0.0}];
-    [aPath addArcWithCenter:(CGPoint){tagLength + radius, radius} radius:radius startAngle:DEGREES_TO_RADIANS(270) endAngle:DEGREES_TO_RADIANS(230) clockwise:NO];
-    [aPath addLineToPoint:(CGPoint){0.0, height / 2}];
-    
-    [aPath moveToPoint:(CGPoint){tagLength / 2, height / 2}];
-    [aPath addArcWithCenter:(CGPoint){tagLength / 2 + self.holeRadius, height / 2} radius:self.holeRadius startAngle:DEGREES_TO_RADIANS(180) endAngle:DEGREES_TO_RADIANS(0) clockwise:YES];
-    
-    UIBezierPath *p2 = [UIBezierPath bezierPathWithCGPath:aPath.CGPath];
-    [p2 applyTransform:CGAffineTransformMakeScale(1, -1)];
-    [p2 applyTransform:CGAffineTransformMakeTranslation(0, height)];
-    [aPath appendPath:p2];
-    
-    // Set the render colors.
-    [self.tagColor setFill];
-    
-    [aPath fill];
-    
-    radius = radius / 2;
-    float padding = self.innerTagPadding;
+    radius -= padding;
     float left = padding * 2;
     UIBezierPath *background = [UIBezierPath bezierPath];
     [background moveToPoint:(CGPoint){tagLength + left + radius, padding}];
@@ -168,12 +170,14 @@ NSString * const AMTagViewNotification = @"AMTagViewNotification";
 
 - (void)drawWithoutTagForRect:(CGRect)rect
 {
-    UIBezierPath* backgroundPath = [UIBezierPath bezierPathWithRoundedRect:rect cornerRadius:self.radius];
-    [self.tagColor setFill];
-    [backgroundPath fill];
+    if (self.innerTagPadding > 0) {
+        UIBezierPath* backgroundPath = [UIBezierPath bezierPathWithRoundedRect:rect cornerRadius:self.radius];
+        [self.tagColor setFill];
+        [backgroundPath fill];
+    }
     
     CGRect inset = CGRectInset(rect, self.innerTagPadding, self.innerTagPadding);
-    UIBezierPath* insidePath = [UIBezierPath bezierPathWithRoundedRect:inset cornerRadius:self.radius / 2];
+    UIBezierPath* insidePath = [UIBezierPath bezierPathWithRoundedRect:inset cornerRadius:self.radius - self.innerTagPadding];
     [self.innerTagColor setFill];
     [insidePath fill];
 }
