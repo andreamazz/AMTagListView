38ffd11433a4ebe1c9257e068eb95adf4591d2f1
Nicholas Solter <nick@emmerge.com>
2016-02-08 16:04:47 +0100

    support for horizontal scrolling of tags instead of vertical; controlled via scrollDirection propery on AMTagListView

--- Source/AMTagListView.h
+++ Source/AMTagListView.h
@@ -23,6 +23,18 @@ typedef NS_ENUM(NSInteger, AMTagAlignment) {
     AMTagAlignmentRight
 };
 
+/** @enum AMScrollDirection
+ *
+ * Enum that specifies the scroll direction of the view
+ */
+typedef NS_ENUM(NSInteger, AMScrollDirection) {
+    /** vertical */
+    AMScrollDirectionVertical,
+    /** horizontal */
+    AMScrollDirectionHorizontal
+};
+
+
 
 /**-----------------------------------------------------------------------------
  * @name AMTagListDelegate
@@ -173,5 +185,11 @@ typedef void (^AMTagListViewTapHandler)(AMTagView*);
  * The tag list alignment. The tags can be aligned to the left or the right.
  */
 @property (nonatomic, assign) AMTagAlignment tagAlignment;
+
+/** Scroll direction
+ *
+ * The direction in which the view scrolls (horizontal or vertical)
+ */
+@property (nonatomic, assign) AMScrollDirection scrollDirection;
 
 @end
